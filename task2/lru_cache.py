1ad4272e19b32d982c667ae0b4c91e67b2044a63
rmukkamala <tony@Rohans-MacBook-Pro.local>
2019-05-27 16:53:05 -0700

    adding lru cache prg

---
+++ task2/lru_cache.py
@@ -0,0 +1,62 @@
+
+class Node(object):
+    def __init__(self, key, value):
+        self.key=key
+        self.value=value
+        self.head=None
+        self.tail=None
+
+class LRU_Cache(object):
+
+    def __init__(self, capacity):
+        # Initialize class variables
+        self.capacity=capacity
+        self.dict=dict()
+        self.head=Node(0,0)
+        self.tail=Node(0,0)
+        self.head.next=self.tail
+        self.tail.prev=self.head
+        
+
+    def get(self, key):
+        # Retrieve item from provided key. Return -1 if nonexistent.
+        if key in self.dict:
+            self._remove(self.dict[key])
+            self._add(self.dict[key])
+            return self.dict[key].value
+        return -1
+
+    def set(self, key, value):
+        # Set the value if the key is not present in the cache. If the cache is at capacity remove the oldest item.
+        if key in self.dict:
+            self._remove(self.dict[key])
+        node=Node(key,value)
+        self._add(node)
+        self.dict[key]=node
+        if len(self.dict) > self.capacity:
+            next=self.head.next
+            self._remove(next)
+            del self.dict[next.key]
+
+    def _add(self, node):
+        prev=self.tail.prev
+        prev.next=node
+        self.tail.prev=node
+        node.prev=prev
+        node.next=self.tail
+
+
+    def _remove(self, node):
+        prev=node.prev
+        next=node.next
+        prev.next=next
+        next.prev=prev
+        
+
+our_cache = LRU_Cache(5)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.get(1)       # returns 1
+our_cache.get(2)       # returns 2
+our_cache.get(3)       # return -1
