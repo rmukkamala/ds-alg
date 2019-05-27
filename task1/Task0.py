afcd63f73f4dbea0f1f83edf19450a852d2d8673
Rohan Mukkamala <tony@Rohans-MacBook-Pro.local>
2019-05-27 10:33:34 -0700

    add task 2 files

--- task1/Task0.py
+++ task1/Task0.py
@@ -3,13 +3,18 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+from os.path import dirname, join
+current_dir = dirname(__file__)
+file_path1=join(current_dir, "./texts.csv")
+file_path2=join(current_dir, "./calls.csv")
+
+with open(file_path1, 'r') as f:
     reader = csv.reader(f)
     print(type(reader))
     texts = list(reader)
     print("First record of texts, {} texts {} at time {}".format(texts[0][0],texts[0][1],texts[0][2]))
 
-with open('calls.csv', 'r') as f:
+with open(file_path2, 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
     print("Last record of calls, {} calls {} at time {}, lasting {} seconds".format(calls[-1][0],calls[-1][1],calls[-1][2],calls[-1][3]))
