d60bf43ce9bf8c870cd1f54f31bc89bad31c3841
rmukkamala <tony@Rohans-MacBook-Pro.local>
2019-05-30 21:24:51 -0700

    adding prg for block chain

--- task2/blockchain.py
+++ task2/blockchain.py
@@ -1,32 +1,52 @@
-
+####Block Chain###
 
 import hashlib
 import datetime
 
 class Block:
-
-    def __init__(self, timestamp, data, previous_hash):
+    def __init__(self, index, timestamp, data, previous_hash):
+      self.index= index
       self.timestamp = timestamp
       self.data = data
       self.previous_hash = previous_hash
       self.hash = self.calc_hash()
-    
-    def get_genesis_block(self):
-        return Block(datetime.datetime.now(),'0','0')
-
-
-    def calc_hash():
+      
+    def calc_hash(self):
       sha = hashlib.sha256()
-      hash_str = "We are going to encode this string of data!".encode('utf-8')
+      hash_str = (str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)).encode('utf-8')
       sha.update(hash_str)
       return sha.hexdigest()
 
-    def add_block(self,):
+##To crete the next block
+def create_next_block(lastblock,data):
+  index=lastblock.index + 1
+  prev_hash=lastblock.hash
+  return Block(index,datetime.datetime.now(),data,prev_hash)
+  
+##To display/visualize the exisiting Block chain 
+def display_blockchain(blockchain):
+  for i in range(0, len(blockchain)):
+    text=[ "Block: " + str(blockchain[i].index), str(blockchain[i].timestamp), str(blockchain[i].data), str(blockchain[i].hash)]
+    maxlen=max( len(s) for s in text)
+    colwidth= maxlen + 2
+    print('+' + '-'*colwidth + '+')
+    for s in text:
+      print('| %-*.*s |' % (maxlen,maxlen,s))
+    print('+' + '-'*colwidth + '+')
+    print('            |')
 
-#print(datetime.datetime.now())
-#newBlockChain= Block()
-# 
-block_chain = [Block.get_genesis_block()]
+#To initialize/create the first/genesis block
+def create_genesis_block():
+  return Block(0,datetime.datetime.now(),'This is Genesis Block','0')
 
-print("The genesis block has been created.")
-print("Hash: %s" % block_chain[0].hash)
+
+#creating list of all the chained blocks
+block_chain = [ create_genesis_block() ]
+
+block_chain.append(create_next_block(block_chain[-1],'This is awesome'))
+block_chain.append(create_next_block(block_chain[-1],'This is bad'))
+block_chain.append(create_next_block(block_chain[-1],'This is great'))
+block_chain.append(create_next_block(block_chain[-1],'This is superb'))
+block_chain.append(create_next_block(block_chain[-1],'This is the best'))
+
+display_blockchain(block_chain)
--- task2/sample.py
+++ task2/sample.py
@@ -1,5 +1,24 @@
+"""
+ 1.	Input:-Number of message with frequency count.
+    2.	Output: - Huffman merge tree.
+    3.	Begin
+    4.	Let Q be the priority queue,
+    5.	Q= {initialize priority queue with frequencies of all symbol or message}
+    6.	Repeat n-1 times 
+    7.	Create a new node Z 
+    8.	X=extract_min(Q)
+    9.	Y=extract_min(Q)
+    10.	Frequency(Z) =Frequency(X) +Frequency(y);
+    11.	Insert (Z, Q)
+    12.	End repeat 
+    13.	Return (extract_min(Q))
+    14.	End.
+
+"""
 
 
+import sys,string
+codes=dict()
 def frequency(str):
     freq=dict()
     for char in str:
@@ -15,13 +34,73 @@ def sort_frequency(freq):
     list_tuples.sort()
     return list_tuples
 
+def buildTree(tuples):
+    while len(tuples)>1:
+        least_two=  tuple(tuples[0:2])
+        rest= tuples[2:]
+        sum_freq=least_two[0][0] + least_two[1][0]
+        tuples= rest + [(sum_freq,least_two)]
+        tuples.sort(key=lambda t: t[0])
+    return tuples[0]
+
+def trimTree (tree) :
+    p = tree[1]                                    # ignore freq count in [0]
+    if type(p) == type("") : 
+        #print(p)
+        return p
+    else : 
+        return (trimTree(p[0]), trimTree(p[1]))
 
 
+def assignCodes (node, pat='') :
+    global codes
+    if type(node) == type("") :
+        codes[node] = pat                # A leaf. set its code
+    else  :                              #
+        assignCodes(node[0], pat+"0")    # Branch point. Do the left branch
+        assignCodes(node[1], pat+"1") 
+
+
+def encode (str) :
+    global codes
+    output = ""
+    for ch in str : output += codes[ch]
+    return output
+
+def decode(tree, str) :
+    output = ""
+    p = tree
+    #print(p)
+    for bit in str :
+        if bit == '0' : 
+            #print(p[0])
+            k = p[0]     # Head up the left branch
+            pass
+        else: 
+            k = p[1]    # or up the right branch
+            pass
+        if type(k) == type(""):
+            output += k              # found a character. Add to output
+            k = tree                 # and restart for next character
+    return output
 
 msg= "The bird is the word"
 hash=frequency(msg)
-print(sort_frequency(hash))
+tuples=sort_frequency(hash)
+#print(tuples)
+tree=buildTree(tuples)
+print(tree)
+trim=trimTree(tree)
+assignCodes(trim)
 
+small = encode(msg)
+print(small)
+original = decode(tree, small)
+print(original)
+
+# print("Original text length", len(msg))
+# print("Requires %d bits. (%d bytes)" % (len(small), (len(small)+7)/8))
+# print("Restored matches original", str == original)
 
 
 # list1 = [(1, 2), (3, 3), (5, 1)]
