3917960a90a6fefd501fa615f526f40883786062
Rohan Mukkamala <rmukkamala@Broadcom.net>
2019-08-02 16:46:26 -0700

    Adding files for task3

--- task3/dutch_flag.py
+++ task3/dutch_flag.py
@@ -1,10 +1,10 @@
 
 """
 Dutch National Flag Problem
-Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal. You're not allowed to use any sorting function that Python provides.
-
-Note: O(n) does not necessarily mean single-traversal. For e.g. if you traverse the array twice, that would still be an O(n) solution but it will not count as single traversal.
-
+Given an input array consisting on only 0, 1, and 2, sort the array in a single traversal. 
+You're not allowed to use any sorting function that Python provides.
+Note: O(n) does not necessarily mean single-traversal. For e.g. if you traverse the array twice, that would still be an O(n) solution 
+but it will not count as single traversal.
 Here is some boilerplate code and test cases to start with:
 """
 
--- task3/max_and_min.py
+++ task3/max_and_min.py
@@ -1,8 +1,8 @@
 
 """
 Max and Min in a Unsorted Array
-In this problem, we will look for smallest and largest integer from a list of unsorted integers. The code should run in O(n) time. Do not use Python's inbuilt functions to find min and max.
-
+In this problem, we will look for smallest and largest integer from a list of unsorted integers. 
+The code should run in O(n) time. Do not use Python's inbuilt functions to find min and max.
 Bonus Challenge: Is it possible to find the max and min in a single traversal?
 Sorting usually requires O(n log n) time Can you come up with a O(n) algorithm (i.e., linear time)?
 """
