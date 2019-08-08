73982bb06242a0946693c7a57ac0585c9f51010a
rmukkamala <tony@Rohans-MacBook-Pro.local>
2019-08-07 22:28:00 -0700

    adding changes for task3

--- task3/http_router.py
+++ task3/http_router.py
@@ -1,57 +1,79 @@
 
 
-# A RouteTrie will store our routes and their associated handlers
-class RouteTrie:
-    def __init__(self, ...):
-        # Initialize the trie with an root node and a handler, this is the root path or home page node
-
-    def insert(self, ...):
-        # Similar to our previous example you will want to recursively add nodes
-        # Make sure you assign the handler to only the leaf (deepest) node of this path
-
-    def find(self, ...):
-        # Starting at the root, navigate the Trie to find a match for this path
-        # Return the handler for a match, or None for no match
-
 # A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
 class RouteTrieNode:
-    def __init__(self, ...):
+    def __init__(self, handler):
         # Initialize the node with children as before, plus a handler
+        self.children={}
+        self.handler=handler
+        self.isEnd=False
 
-    def insert(self, ...):
-        # Insert the node as before
+    """ def insert(self):
+        # Insert the node as before  """
 
+# A RouteTrie will store our routes and their associated handlers
+class RouteTrie:
+    def __init__(self, handler):
+        # Initialize the trie with an root node and a handler, this is the root path or home page node
+        self.root=RouteTrieNode(handler)
+
+    def insert(self, path_list, handler):
+        # Similar to our previous example you will want to recursively add nodes
+        # Make sure you assign the handler to only the leaf (deepest) node of this path
+        curr=self.root
+        for entry in path_list:
+            if entry not in curr.children:  
+                curr.children[entry]=RouteTrieNode(None)
+            curr=curr.children[entry]
+        curr.isEnd=True
+        curr.handler=handler
+
+    def find(self,path_list):
+        # Starting at the root, navigate the Trie to find a match for this path
+        # Return the handler for a match, or None for no match
+        curr=self.root
+        for entry in path_list:
+            if entry not in curr.children:
+                return None
+            curr=curr.children[entry]
+        return curr.handler
 
 # The Router class will wrap the Trie and handle 
 class Router:
-    def __init__(self, ...):
+    def __init__(self, handler):
         # Create a new RouteTrie for holding our routes
         # You could also add a handler for 404 page not found responses as well!
+        self.router=RouteTrie(handler)
 
-    def add_handler(self, ...):
+    def add_handler(self, path, handler):
         # Add a handler for a path
         # You will need to split the path and pass the pass parts
         # as a list to the RouteTrie
+        self.router.insert(self.split_path(path),handler)
 
-    def lookup(self, ...):
+    def lookup(self,full_path):
         # lookup path (by parts) and return the associated handler
         # you can return None if it's not found or
         # return the "not found" handler if you added one
         # bonus points if a path works with and without a trailing slash
         # e.g. /about and /about/ both return the /about handler
+        if full_path=='/':
+            return 'root handler'
+        return self.router.find(self.split_path(full_path))
 
 
-    def split_path(self, ...):
+    def split_path(self, path):
         # you need to split the path into parts for 
         # both the add_handler and loopup functions,
         # so it should be placed in a function here
+        return path.split('/')
 
 
 
 # Here are some test cases and expected outputs you can use to test your implementation
 
 # create the router and add a route
-router = Router("root handler", "not found handler") # remove the 'not found handler' if you did not implement this
+router = Router("root handler") # remove the 'not found handler' if you did not implement this
 router.add_handler("/home/about", "about handler")  # add a route
 
 # some lookups with the expected output
